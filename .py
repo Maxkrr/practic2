f7838dcd3adc3123a004cd201db0a9520ab8a4ed
Maxkrr <krivosheevmax2003@gmail.com>
2025-09-29 19:16:43 +0300

    Update .py

--- .py
+++ .py
@@ -1,1 +1,38 @@
+# Хэш - таблицы:
+class HashTable:
+    def __init__(self, size=10):
+        self.size = size
+        self.table = [[] for _ in range(size)]
 
+    def hash_function(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        for kvp in self.table[index]:
+            if kvp[0] == key:
+                kvp[1] = value
+                return
+        self.table[index].append([key, value])
+
+    def search(self, key):
+        index = self.hash_function(key)
+        for kvp in self.table[index]:
+            if kvp[0] == key:
+                return kvp[1]
+        return None
+
+    def delete(self, key):
+        index = self.hash_function(key)
+        for i, kvp in enumerate(self.table[index]):
+            if kvp[0] == key:
+                del self.table[index][i]
+                return
+
+# Пример использования
+ht = HashTable()
+ht.insert('apple', 5)
+print(ht.search('apple'))  # 5
+ht.delete('apple')
+print(ht.search('apple'))  # None
+# Куча Фибоначчи:

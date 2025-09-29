c45d01a2ee8109710018ed11225df1d6c7f793d4
Maxkrr <krivosheevmax2003@gmail.com>
2025-09-29 19:24:53 +0300

    Update .py

--- .py
+++ .py
@@ -36,3 +36,48 @@ print(ht.search('apple'))  # 5
 ht.delete('apple')
 print(ht.search('apple'))  # None
 # Куча Фибоначчи:
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = self.right = self
+        self.child = self.parent = None
+        self.degree = 0
+
+class FibHeap:
+    def __init__(self):
+        self.min = None
+
+    def insert(self, x):
+        if not self.min:
+            self.min = x
+        else:
+            x.left, x.right = self.min, self.min.right
+            self.min.right.left = x
+            self.min.right = x
+            if x.key < self.min.key:
+                self.min = x
+
+    def extract_min(self):
+        z = self.min
+        if z:
+            if z.child:
+                c = z.child
+                children = []
+                while True:
+                    children.append(c)
+                    c = c.right
+                    if c == z.child:
+                        break
+                for child in children:
+                    child.parent = None
+                    child.left.right = child.right
+                    child.right.left = child.left
+                    child.left = self.min
+                    child.right = self.min.right
+                    self.min.right.left = child
+                    self.min.right = child
+            z.left.right = z.right
+            z.right.left = z.left
+            self.min = z.right if z != z.right else None
+        return z.key if z else None
+        

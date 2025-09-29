f5b6516cb760781a649d8fe25289c133620befb9
Maxkrr <krivosheevmax2003@gmail.com>
2025-09-29 19:19:36 +0300

    Update .java

--- .java
+++ .java
@@ -1,1 +1,15 @@
+// Хэш-таблицы:
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1, "apple");
+        System.out.println(map.get(1)); // apple
+
+        map.remove(1);
+        System.out.println(map.get(1)); // null
+    }
+}
+// Куча Фибоначчи:
